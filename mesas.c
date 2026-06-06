d7473b35e062cc9babbbddd98ffad823bc75a2c7
remojs <thiagozambonini24@gmail.com>
2026-06-06 17:53:30 -0300

    cambios gitignore

--- mesas.c
+++ mesas.c
@@ -108,7 +108,7 @@ int bajaMesa(Mesa** mesas, int* cantidad) {
 
 // int → 1 si encontró y modificó, 0 si no encontró
 int modificarMesa(Mesa* mesas, int cantidad) {
-
+    
 }
 
 // int → 1 si encontró y mostró, 0 si no existe
